d6f57a595ec904af19ed2533307efad0d31810bd
Raphael <raphael.asantos@outlook.com>
2024-09-16 11:19:18 -0300

    Alteração 1 portfolio

--- assets/js/johndoe.js
+++ assets/js/johndoe.js
@@ -13,81 +13,229 @@
 */
 
 // smooth scroll
-$(document).ready(function(){
-  $(".navbar .nav-link").on('click', function(event) {
-      if (this.hash !== "") {
-          event.preventDefault();
-          var hash = this.hash;
+$(document).ready(function () {
+  $(".navbar .nav-link").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
 
-          // Ajuste o valor de offset conforme a altura do seu cabeçalho
-          var offset = 80; // Altura do cabeçalho fixo (ajuste conforme necessário)
+      // Ajuste o valor de offset conforme a altura do seu cabeçalho
+      var offset = 80; // Altura do cabeçalho fixo (ajuste conforme necessário)
 
-          $('html, body').animate({
-              scrollTop: $(hash).offset().top - offset
-          }, 700, function(){
-              window.location.hash = hash;
-          });
-      } 
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - offset
+      }, 700, function () {
+        window.location.hash = hash;
+      });
+    }
   });
 });
 
+//Blog mostrar projetos
+const posts = [
+  {
+     title: "Vaso Sanitário Automatizado",
+     image: "assets/imgs/blog1.jpg",
+     content: "Projeto de vaso sanitário automatizado com Arduino que utiliza sensores para levantar a tampa e acionar a descarga automaticamente, proporcionando maior conveniência e higiene.",
+     link: "projeto1.html",
+     date: "2018-12-01",
+     tecnologias: ["Arduino"]
+   },
+   {
+     title: "Projeto 2",
+     image: "assets/imgs/blog2.jpg",
+     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+     link: "projeto2.html",
+     date: "2023-08-10",
+     tecnologias: ["PHP"]
+   },
+   {
+     title: "Projeto 3",
+     image: "assets/imgs/blog3.jpg",
+     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+     link: "projeto3.html",
+     date: "2023-07-05",
+     tecnologias: ["Python"]
+   },
+   {
+     title: "Projeto 4",
+     image: "assets/imgs/branding-1.jpg",
+     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+     link: "projeto4.html",
+     date: "2023-06-25",
+     tecnologias: ["Arduino", "HTML"]
+   },
+   {
+     title: "Projeto 5",
+     image: "assets/imgs/branding-2.jpg",
+     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+     link: "projeto5.html",
+     date: "2023-05-30",
+     tecnologias: ["Python"]
+   },
+   {
+     title: "Projeto 6",
+     image: "assets/imgs/branding-3.jpg",
+     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+     link: "projeto6.html",
+     date: "2023-04-12",
+     tecnologias: ["HTML"]
+   }
+ ];
+ 
+ const postsPerPage = 3; // Definindo 3 postagens por página
+ let currentPage = 1; // Iniciando na página 1
+ 
+ function sortPostsByDate() {
+   posts.sort((a, b) => new Date(b.date) - new Date(a.date)); // Classifica do mais recente para o mais antigo
+ }
+ 
+ function filterByTechnology() {
+   const checkedTechs = Array.from(document.querySelectorAll('#technologyFilter input:checked'))
+                             .map(checkbox => checkbox.value);
+ 
+   let filteredPosts = posts;
+   if (checkedTechs.length > 0) {
+     filteredPosts = posts.filter(post => 
+       checkedTechs.every(tech => post.tecnologias.includes(tech))
+     );
+   } else {
+     // Se nenhum filtro estiver selecionado, não aplicar filtragem
+     filteredPosts = posts;
+   }
+ 
+   // Ajusta a página atual se necessário
+   const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
+   if (currentPage > totalPages) {
+     currentPage = 1; // Redefine a página atual para 1 se a página atual for maior que o total de páginas disponíveis
+   }
+ 
+   // Atualiza a renderização com os posts filtrados
+   renderPosts(filteredPosts);
+   renderPagination(filteredPosts);
+ }
+ 
+ function renderPosts(filteredPosts = posts) { // Use posts filtrados ou todos
+   const blogPosts = document.getElementById('blog-posts');
+   blogPosts.innerHTML = ""; // Limpa o conteúdo existente
+ 
+   const start = (currentPage - 1) * postsPerPage;
+   const end = start + postsPerPage;
+   const paginatedPosts = filteredPosts.slice(start, end);
+ 
+   // Renderiza as postagens na página atual
+   paginatedPosts.forEach(post => {
+     const postHtml = `
+           <div class="blog-card">
+               <div class="img-holder">
+                   <img src="${post.image}" alt="${post.title}">
+               </div>
+               <div class="content-holder">
+                  <h6 class="title">
+                      <a href="${post.link}">${post.title}</a>
+                  </h6>
+                   <p>${post.content}</p>
+                   <a href="${post.link}" class="read-more">Veja mais <i class="ti-angle-double-right"></i></a>
+               </div>
+           </div>
+       `;
+     blogPosts.innerHTML += postHtml;
+   });
+ }
+ 
+ function renderPagination(filteredPosts = posts) { // Use posts filtrados ou todos
+   const pagination = document.getElementById('pagination');
+   pagination.innerHTML = ""; // Limpa a paginação atual
+ 
+   const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
+ 
+   // Cria botões de paginação
+   for (let i = 1; i <= totalPages; i++) {
+     const pageButton = document.createElement('button');
+     pageButton.innerText = i;
+     pageButton.classList.add('page-btn');
+     if (i === currentPage) {
+       pageButton.classList.add('active');
+     }
+     pageButton.addEventListener('click', () => {
+       currentPage = i;
+       renderPosts(filteredPosts);
+       renderPagination(filteredPosts);
+     });
+     pagination.appendChild(pageButton);
+   }
+ }
+ 
+ // Adiciona eventos de mudança para os checkboxes
+ document.querySelectorAll('#technologyFilter input').forEach(checkbox => {
+   checkbox.addEventListener('change', filterByTechnology);
+ });
+ 
+ // Inicializa a primeira página e a paginação
+ sortPostsByDate(); // Ordena os posts por data antes de renderizar
+ renderPosts();
+ renderPagination();
+ 
 
-
-// protfolio filters
-$(window).on("load", function() {
-    var t = $(".portfolio-container");
-    t.isotope({
-        filter: ".new",
-        animationOptions: {
-            duration: 750,
-            easing: "linear",
-            queue: !1
-        }
-    }), $(".filters a").click(function() {
-        $(".filters .active").removeClass("active"), $(this).addClass("active");
-        var i = $(this).attr("data-filter");
-        return t.isotope({
-            filter: i,
-            animationOptions: {
-                duration: 750,
-                easing: "linear",
-                queue: !1
-            }
-        }), !1
-    });
+// portfolio filters
+$(window).on("load", function () {
+  var t = $(".portfolio-container");
+  t.isotope({
+    filter: ".new",
+    animationOptions: {
+      duration: 750,
+      easing: "linear",
+      queue: !1
+    }
+  }), $(".filters a").click(function () {
+    $(".filters .active").removeClass("active"), $(this).addClass("active");
+    var i = $(this).attr("data-filter");
+    return t.isotope({
+      filter: i,
+      animationOptions: {
+        duration: 750,
+        easing: "linear",
+        queue: !1
+      }
+    }), !1
+  });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   const projects = [
-    { img: "assets/imgs/portifolio/prog_viagens1.png", 
-      title: "Programando Viagens V1", 
-      desc: "Desenvolvido como parte do primeiro semestre do curso de Análise e Desenvolvimento de Sistemas, Turma A Noturno. O objetivo é criar um site para planejamento e organização de viagens.", 
-      projectLink: "https://programandoviagens.vercel.app", 
-      githubLink: "https://github.com/raphaelsantos141/ProgramandoViagens" },
+    {
+      img: "assets/imgs/portifolio/prog_viagens1.png",
+      title: "Programando Viagens V1",
+      desc: "Desenvolvido como parte do primeiro semestre do curso de Análise e Desenvolvimento de Sistemas, Turma A Noturno. O objetivo é criar um site para planejamento e organização de viagens.",
+      projectLink: "https://programandoviagens.vercel.app",
+      githubLink: "https://github.com/raphaelsantos141/ProgramandoViagens"
+    },
 
-      { img: "assets/imgs/portifolio/acrilwoodind.png", 
-        title: "Acrilwood Ind", 
-        desc: "Criado como uma iniciativa pessoal para a empresa onde trabalho para colocar em prática HTML, CSS e JavaScript. A partir de um template pré-existente, explorei como cada parte funcionava e personalizei a interface para torná-la mais agradável e funcional.", 
-        projectLink: "https://acrilwoodind.vercel.app", 
-        githubLink: "https://github.com/raphaelsantos141/Acrilwood-Ind" },
+    {
+      img: "assets/imgs/portifolio/acrilwoodind.png",
+      title: "Acrilwood Ind",
+      desc: "Criado como uma iniciativa pessoal para a empresa onde trabalho para colocar em prática HTML, CSS e JavaScript. A partir de um template pré-existente, explorei como cada parte funcionava e personalizei a interface para torná-la mais agradável e funcional.",
+      projectLink: "https://acrilwoodind.vercel.app",
+      githubLink: "https://github.com/raphaelsantos141/Acrilwood-Ind"
+    },
 
 
-        
-      // Adicione mais projetos conforme necessário
+
+    // Adicione mais projetos conforme necessário
   ];
 
   const projectsPerPage = 6;
   let currentPage = 0;
 
   function renderProjects() {
-      const start = currentPage * projectsPerPage;
-      const end = start + projectsPerPage;
-      const projectsToShow = projects.slice(start, end);
+    const start = currentPage * projectsPerPage;
+    const end = start + projectsPerPage;
+    const projectsToShow = projects.slice(start, end);
 
-      $('#portfolio-grid').empty();
+    $('#portfolio-grid').empty();
 
-      projectsToShow.forEach(project => {
-          $('#portfolio-grid').append(`
+    projectsToShow.forEach(project => {
+      $('#portfolio-grid').append(`
               <div class="project">
                   <img src="${project.img}" alt="${project.title}">
                   <div class="project-info">
@@ -100,21 +248,21 @@ $(document).ready(function() {
                   </div>
               </div>
           `);
-      });
+    });
   }
 
-  $('#prev-button').on('click', function() {
-      if (currentPage > 0) {
-          currentPage--;
-          renderProjects();
-      }
+  $('#prev-button').on('click', function () {
+    if (currentPage > 0) {
+      currentPage--;
+      renderProjects();
+    }
   });
 
-  $('#next-button').on('click', function() {
-      if ((currentPage + 1) * projectsPerPage < projects.length) {
-          currentPage++;
-          renderProjects();
-      }
+  $('#next-button').on('click', function () {
+    if ((currentPage + 1) * projectsPerPage < projects.length) {
+      currentPage++;
+      renderProjects();
+    }
   });
 
   // Render initial set of projects
@@ -124,7 +272,7 @@ $(document).ready(function() {
 
 
 //Imprimir Curriculo
-document.getElementById('printButton').addEventListener('click', function() {
+document.getElementById('printButton').addEventListener('click', function () {
   // URL do PDF
   var pdfUrl = '/assets/pdfs/Curriculo_Raphael.pdf';
 
@@ -132,101 +280,100 @@ document.getElementById('printButton').addEventListener('click', function() {
   var printWindow = window.open(pdfUrl, '_blank');
 
   // Adiciona um evento de carregamento para acionar a impressão
-  printWindow.onload = function() {
-      printWindow.print();
+  printWindow.onload = function () {
+    printWindow.print();
   };
 });
 
-
 // google maps
 function initMap() {
-// Styles a map in night mode.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.674, lng: -73.945},
-        zoom: 12,
-        scrollwheel:  false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-      styles: [
-        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{color: '#263c3f'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#6b9a76'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{color: '#38414e'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#212a37'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9ca5b3'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{color: '#746855'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#1f2835'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#f3d19c'}]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [{color: '#2f3948'}]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#17263c'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#515c6d'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [{color: '#17263c'}]
-        }
-      ]
-    });
+  // Styles a map in night mode.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 40.674, lng: -73.945 },
+    zoom: 12,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    styles: [
+      { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+      }
+    ]
+  });
 }
